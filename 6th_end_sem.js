41dd44459359949e0d92db9c14be50d806a04fd5
Satyajit <satyajit1470@gmail.com>
2024-05-14 18:45:44 +0530

    Add script to download answer sheets for 6th end semester

---
+++ 6th_end_sem.js
@@ -0,0 +1,30 @@
+async function downloadImage(imageSrc, nameOfDownload = "my-image.png") {
+    const response = await fetch(imageSrc);
+  
+    const blobImage = await response.blob();
+  
+    const href = URL.createObjectURL(blobImage);
+  
+    const anchorElement = document.createElement("a");
+    anchorElement.href = href;
+    anchorElement.download = nameOfDownload;
+  
+    document.body.appendChild(anchorElement);
+    anchorElement.click();
+  
+    document.body.removeChild(anchorElement);
+    window.URL.revokeObjectURL(href);
+  }
+  
+  for (var i = 1; i <= 24; i++) {
+    if (i < 10) {
+      let url = `https://evaluation.kiitresults.com/end2024stview/cs/GenerateImage.aspx?pgno=00${i}|du|${i}`;
+      downloadImage(url, `00${i}.png`);
+      console.log(url);
+    } else {
+      let url = `https://evaluation.kiitresults.com/end2024stview/cs/GenerateImage.aspx?pgno=0${i}|du|${i}`;
+      downloadImage(url, `0${i}.png`);
+      console.log(url);
+    }
+  }
+  
